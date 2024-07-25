f0c1e2941a2551a34ba54d6e9d66a24c213d94d2
keshav <keshavvijay2000@gmail.com>
2024-07-26 00:57:28 +0530

    solution-ui-fixes

--- src/components/Solution/heroSection.js
+++ src/components/Solution/heroSection.js
@@ -5,77 +5,6 @@ const HeroSection = () => {
   return (
     <InView triggerOnce threshold={0.5}>
       {({ inView, ref, entry }) => (
-        // <div>
-        //   <div className="  flex justify-center items-center" ref={ref}>
-        //     <div
-        //       className={`w-full max-w-[80%] flex flex-col md:flex-row justify-around items-center h-screen p-4 ${
-        //         inView ? "animate-scale-opacity-z" : ""
-        //       }`}
-        //     >
-        //       <div className="text-center md:text-left">
-        //         <h1 className="text-2xl md:text-3xl font-bold mb-4 text-[#62D89F] font-trebuchet">
-        //           Creating Endless Lending Possibilities
-        //         </h1>
-        //         <p className="font-thin text-base md:text-md text-white max-w-lg mx-0">
-        //           Finmed is spearheading a revolution in both the financial and
-        //           healthcare industries. Our solutions empower financial
-        //           institutions and lenders with access to a secure platform.
-        //           With Finmed, you can accurately establish the value of
-        //           healthcare insurance receivables while streamlining approval
-        //           and onboarding processes.
-        //         </p>
-        //       </div>
-        //       <div className="shade-bg"></div>
-        //       <div className="grid grid-cols-2 md:grid-cols-3  md:py-16 gap-8 md:gap-20 max-w-xl mx-auto md:mx-0">
-        //         <Image
-        //           src="/images/step1.svg"
-        //           alt="Icon 1"
-        //           width={40}
-        //           height={40}
-        //           className="w-10 h-10 md:w-10 md:h-10 lg:w-12 lg:h-12 object-contain"
-        //         />
-        //         <Image
-        //           src="/images/step2.svg"
-        //           alt="Icon 2"
-        //           width={40}
-        //           height={40}
-        //           className="w-10 h-10 md:w-10 md:h-10 lg:w-12 lg:h-12 object-contain"
-        //         />
-        //         <Image
-        //           src="/images/step3.svg"
-        //           alt="Icon 3"
-        //           width={40}
-        //           height={40}
-        //           className="w-10 h-10 md:w-10 md:h-10 lg:w-12 lg:h-12 object-contain"
-        //         />
-        //         <Image
-        //           src="/images/step4.svg"
-        //           alt="Icon 4"
-        //           width={40}
-        //           height={40}
-        //           className="w-10 h-10 md:w-10 md:h-10 lg:w-12 lg:h-12 object-contain"
-        //         />
-        //         <Image
-        //           src="/images/step5.svg"
-        //           alt="Icon 5"
-        //           width={40}
-        //           height={40}
-        //           className="w-10 h-10 md:w-10 md:h-10 lg:w-12 lg:h-12 object-contain"
-        //         />
-        //         <Image
-        //           src="/images/step6.svg"
-        //           alt="Icon 6"
-        //           width={40}
-        //           height={40}
-        //           className="w-10 h-10 md:w-10 md:h-10 lg:w-12 lg:h-12 object-contain"
-        //         />
-        //       </div>
-        //     </div>
-        //   </div>
-        //   <div className="py-32 px-8 flex justify-center">
-        //     <div className="separator"></div>
-        //   </div>
-        // </div>
         <div>
           <div className="flex justify-center items-center" ref={ref}>
             <div
@@ -87,7 +16,7 @@ const HeroSection = () => {
                 <h1 className="text-2xl md:text-3xl font-bold mb-4 text-[#62D89F] font-trebuchet">
                   Creating Endless Lending Possibilities
                 </h1>
-                <p className="font-thin text-base md:text-md text-white max-w-lg mx-auto md:mx-0">
+                <p className="font-thin text-base md:text-sm text-white max-w-md mx-auto md:mx-0">
                   Finmed is spearheading a revolution in both the financial and
                   healthcare industries. Our solutions empower financial
                   institutions and lenders with access to a secure platform.
